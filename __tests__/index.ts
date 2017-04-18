6d3bf6ef901f49a664e54d808276cbf67c43a04f
Vu Tran <vutran@dropbox.com>
2017-04-18 14:28:04 -0700

    linting

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,101 +2,101 @@ import { build_fn } from 'src/index';
 
 describe('project', () => {
     it('should run the filter function', () => {
-        var test_data = { haystack: 'this is a test' };
-        var search_string = 'test';
-        var filter = build_fn(search_string);
-        var filter2 = build_fn('foo');
-        var result = filter(test_data);
-        var result2 = filter2(test_data);
+        const test_data = { haystack: 'this is a test' };
+        const search_string = 'test';
+        const filter = build_fn(search_string);
+        const filter2 = build_fn('foo');
+        const result = filter(test_data);
+        const result2 = filter2(test_data);
         expect(result).toBe(true);
         expect(result2).toBe(false);
     });
 
     it('should filter string results on basic haystack search', () => {
-        var test_data = [
-            {'haystack': 'this is a test'}
+        const test_data = [
+            { haystack : 'this is a test' },
         ];
 
-        var search_string:string = 'test';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'test';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
-        expect(result[0].haystack).toBe(test_data[0]['haystack']);
+        expect(result[0].haystack).toBe('this is a test');
     });
 
     it('should filter objects based on integer equality', () => {
-        var test_data = [
-            {'name':'Han', 'age': 35},
-            {'name':'Leia', 'age': 21}
+        const test_data = [
+            { name: 'Han', age: 35 },
+            { name: 'Leia', age: 21 },
         ];
 
-        var search_string:string = 'age:<30';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'age:<30';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('Leia');
     });
 
     it('should support emoji searches.', () => {
-        var test_data = [
-            {'haystack': 'this is a test 🔮   '}
+        const test_data = [
+            { haystack: 'this is a test 🔮' },
         ];
 
-        var search_string:string = '🔮';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = '🔮';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
-        expect(result[0].haystack).toBe(test_data[0]['haystack']);
+        expect(result[0].haystack).toBe('this is a test 🔮');
     });
 
 });
 
 describe('feature_macros', () => {
     it('should expand a macro.', () => {
-        var test_data = [
-            {'is_dir':true, 'path':'myfolder/'},
-            {'is_dir':false, 'path':'myfolder/myfile.txt'}
+        const test_data = [
+            { is_dir: true, path: 'myfolder/' },
+            { is_dir: false, path: 'myfolder/myfile.txt' },
         ];
 
-        var search_string:string = 'type:folder';
-        var macro_func = (key:string, arg_list:Array<string>) => {
-            if (arg_list && arg_list.indexOf("folder") != -1) {
-                return ["is_dir", ["true"]];
+        const search_string = 'type:folder';
+        const macro_func = (key: string, arg_list: Array<string>) => {
+            if (arg_list && arg_list.indexOf('folder') !== -1) {
+                return ['is_dir', ['true']];
             }
         };
 
-        var filter = build_fn(search_string, {
-            macros: { 'type': macro_func }
+        const filter = build_fn(search_string, {
+            macros: { type: macro_func },
         });
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].is_dir).toBe(true);
     });
 
     it('should expand a haystack macro that uses a normal macro.', () => {
-        var test_data = [
-            {'is_dir':true, 'path':'myfolder/'},
-            {'is_dir':false, 'path':'myfolder/myfile.txt'}
+        const test_data = [
+            { is_dir: true, path: 'myfolder/' },
+            { is_dir: false, path: 'myfolder/myfile.txt' },
         ];
 
-        var search_string:string = '/my';
+        const search_string = '/my';
 
-        var macro_func = (key:string, arg_list:Array<string>) => {
-            if (arg_list && arg_list.indexOf("folder") != -1) {
-                return ["is_dir", ["true"]];
+        const macro_func = (key: string, arg_list: Array<string>) => {
+            if (arg_list && arg_list.indexOf('folder') !== -1) {
+                return ['is_dir', ['true']];
             }
         };
 
-        var haystack_macro_func = (key:string): any => {
-            let conditions:Array<any> = [];
-            let remaining_haystack:Array<string> = [];
+        const haystack_macro_func = (key: string): any => {
+            const conditions: Array<any> = [];
+            const remaining_haystack: Array<string> = [];
 
-            if (key[0] == '/') {
+            if (key[0] === '/') {
                 conditions.push('is_dir:true');
                 conditions.push('path:~' + key.slice(1));
             }
@@ -104,12 +104,12 @@ describe('feature_macros', () => {
             return [conditions, remaining_haystack];
         };
 
-        var filter = build_fn(search_string, {
-            macros: { 'type': macro_func },
-            haystack_macros: {'/.*': haystack_macro_func },
+        const filter = build_fn(search_string, {
+            macros: { type: macro_func },
+            haystack_macros: { '/.*': haystack_macro_func },
         });
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].is_dir).toBe(true);
@@ -119,27 +119,27 @@ describe('feature_macros', () => {
 
 describe('feature dig_into_object', () => {
     it('match on child key', () => {
-        var test_data = [
+        const test_data = [
             {
-                "host_id": 54321,
-                "build_history": {
-                    "timestamp": 1411075727,
-                    "version": "Dropbox-mac-3.1.213"
-                }
+                host_id: 54321,
+                build_history: {
+                    timestamp: 1411075727,
+                    version: 'Dropbox-mac-3.1.213',
+                },
             },
             {
-                "host_id": 12345,
-                "build_history": {
-                    "timestamp": 1411075729,
-                    "version": "Dropbox-linux-3.1.213"
-                }
-            }
+                host_id: 12345,
+                build_history: {
+                    timestamp: 1411075729,
+                    version: 'Dropbox-linux-3.1.213',
+                },
+            },
         ];
 
-        var search_string:string = "build_history.version:%mac";
-        var filter = build_fn(search_string);
+        const search_string = 'build_history.version:%mac';
+        const filter = build_fn(search_string);
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].host_id).toBe(54321);
@@ -148,117 +148,119 @@ describe('feature dig_into_object', () => {
 
 describe('compose types and conditions', () => {
     it('allow and conditions', () => {
-        var test_data = [
+        const test_data = [
             {
-                name: "Han Solo",
-                points: 200
+                name: 'Han Solo',
+                points: 200,
             },
             {
-                name: "Leia Organa",
-                points: 500
-            }
+                name: 'Leia Organa',
+                points: 500,
+            },
         ];
 
-        var search_string:string = "points:&,<400,>100";
-        var filter = build_fn(search_string);
+        const search_string = 'points:&,<400,>100';
+        const filter = build_fn(search_string);
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
-        expect(result[0].name).toBe("Han Solo");
+        expect(result[0].name).toBe('Han Solo');
     });
 
     it('allow or conditions', () => {
-        var test_data = [
+        const test_data = [
             {
-                name: "Han Solo",
-                points: 200
+                name: 'Han Solo',
+                points: 200,
             },
             {
-                name: "Leia Organa",
-                points: 500
-            }
+                name: 'Leia Organa',
+                points: 500,
+            },
         ];
 
-        var search_string:string = "points:|,>400,<100";
-        var filter = build_fn(search_string);
+        const search_string = 'points:|,>400,<100';
+        const filter = build_fn(search_string);
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
-        expect(result[0].name).toBe("Leia Organa");
+        expect(result[0].name).toBe('Leia Organa');
     });
 
     it('allow item in list conditions', () => {
-        var test_data = [
-            { 'name': 'even',
-              'numbers': [2,4,6,8,10]
+        const test_data = [
+            {
+                name: 'even',
+                numbers: [2, 4, 6, 8, 10],
             },
-            { 'name': 'odd',
-              'numbers': [1,3,5,7,9]
-            }
+            {
+                name: 'odd',
+                numbers: [1, 3, 5, 7, 9],
+            },
         ];
 
-        var search_string:string = "numbers:$2";
-        var filter = build_fn(search_string);
+        const search_string = 'numbers:$2';
+        const filter = build_fn(search_string);
 
-        var result = test_data.filter(filter);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('even');
     });
 
     it('allow not conditions', () => {
-        var test_data = [
-            {'name': 'han'},
-            {'name': 'luke'}
+        const test_data = [
+            { name: 'han' },
+            { name: 'luke' },
         ];
 
-        var search_string:string = 'name:!luke';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'name:!luke';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('han');
     });
 
     it('allow equal conditions', () => {
-        var test_data = [
-            {'name': 'han'},
-            {'name': 'luke'}
+        const test_data = [
+            { name: 'han' },
+            { name: 'luke' },
         ];
 
-        var search_string:string = 'name:=luke';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'name:=luke';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('luke');
     });
 
     it('allow regex conditions', () => {
-        var test_data = [
-            {'name': 'han'},
-            {'name': 'luke'}
+        const test_data = [
+            { name: 'han' },
+            { name: 'luke' },
         ];
 
-        var search_string:string = 'name:~^..ke$';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'name:~^..ke$';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('luke');
     });
 
     it('allow indexOf conditions', () => {
-        var test_data = [
-            {'name': 'han'},
-            {'name': 'luke'}
+        const test_data = [
+            { name: 'han' },
+            { name: 'luke' },
         ];
 
-        var search_string:string = 'name:%lu';
-        var filter = build_fn(search_string);
-        var result = test_data.filter(filter);
+        const search_string = 'name:%lu';
+        const filter = build_fn(search_string);
+        const result = test_data.filter(filter);
 
         expect(result.length).toBe(1);
         expect(result[0].name).toBe('luke');
